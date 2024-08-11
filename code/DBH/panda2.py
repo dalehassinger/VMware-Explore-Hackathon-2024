275f399c47723f2df914aa80e17a9544cc0fe079
Dale Hassinger <63500175+dalehassinger@users.noreply.github.com>
2024-08-11 08:29:58 -0400

    08-11-2024

--- code/DBH/panda2.py
+++ code/DBH/panda2.py
@@ -2,4 +2,4 @@ import pandas as pd
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 
-df = pd.read_csv("supermarket_sales.csv")
+df = pd.read_csv("/Users/dalehassinger/Documents/GitHub/PS-TAM-Lab/RVTools/RVTools_export_all_2024-06-28_04.55.40/RVTools_tabvInfo.csv")
